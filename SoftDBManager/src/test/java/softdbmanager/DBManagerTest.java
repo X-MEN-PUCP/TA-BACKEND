02c592eb6c5314b76a931933e55e7e1c96299d98
Joker <sfarfan777@gmail.com>
2025-05-09 16:52:58 -0500

    Implementacion del modulo DBManager

---
+++ SoftDBManager/src/test/java/softdbmanager/DBManagerTest.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package softdbmanager;
+
+import java.sql.Connection;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author salva
+ */
+public class DBManagerTest {
+    
+    public DBManagerTest() {
+    }
+
+    
+    @org.junit.jupiter.api.Test
+    public void testGetInstance() {
+        System.out.println("getInstance");                
+        DBManager dBManager = DBManager.getInstance();
+        assertNotNull(dBManager);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testGetConnection() {
+        System.out.println("getConnection");  
+        System.out.println("Consiguiendo Instancia");
+        DBManager dBManager = DBManager.getInstance();
+        System.out.println("Instancia conseguida");
+        System.out.println("Consiguiendo conexion");
+        Connection conexion = dBManager.getConnection();
+        
+        assertNotNull(conexion);
+    }
+    
+}
---
+++ SoftDBManager/src/test/java/softdbmanager/util/CifradoTest.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package softdbmanager.util;
+
+import java.util.Properties;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author salva
+ */
+public class CifradoTest {
+    //Properties properties = new Properties();
+    public CifradoTest() {
+    }
+    
+    Cifrado cifrado = new Cifrado();
+    
+    @Test
+    public void testCifrarMD5() {
+        String clave = "01102003Safp";
+        String claveCifrada;
+        claveCifrada = cifrado.cifrarMD5(clave);
+        System.out.println("Clave Cifrada>"+claveCifrada+"<");
+        String claveDecifrada;
+        claveDecifrada = cifrado.descifrarMD5(claveCifrada);
+        System.out.println("Clave decifrada>"+claveDecifrada+"<");
+        
+    }
+
+
+    
+}
